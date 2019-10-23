155556a41593c7c8bff4b905664e9c052e8ff0c7
tanayuk <yuuki.tnk@gmail.com>
2019-10-23 18:32:39 +0900

    add patch and delete operation for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,22 @@
 const Post = require("../models/post");
 const validationHandler = require("../validations/validationHandler");
 
-exports.index = (req, res) => {
-    res.send({ message: "hi" });
+exports.index = async (req, res, next) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        res.send(posts);
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.show = async (req, res, next) => {
+    try {
+        const post = await Post.findOne({ _id: req.params.id });
+        res.send(post);
+    } catch (err) {
+        next(err);
+    }
 };
 
 exports.store = async (req, res, next) => {
@@ -13,8 +27,34 @@ exports.store = async (req, res, next) => {
         post.description = req.body.description;
         post.image = req.file.filename;
         post = await post.save();
-        
+
         res.send(post);
+    } catch (err) {
+        next(err)
+    }
+};
+
+exports.update = async (req, res, next) => {
+    try {
+        validationHandler(req);
+
+        let post = await Post.findById(req.params.id);
+        post.description = req.body.description;
+        post = await post.save();
+
+        res.send(post);
+    } catch (err) {
+        next(err)
+    }
+};
+
+exports.delete = async (req, res, next) => {
+    try {
+
+        let post = await Post.findById(req.params.id);
+        post = await post.delete();
+
+        res.send({message: "success"});
     } catch (err) {
         next(err)
     }
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,9 @@ const postController = require("../controllers/postController");
 const { hasDescription } = require('../validations/validators');
 
 router.get("/", postController.index);
+router.get("/:id", postController.show);
 router.post("/", uploadImage('posts').single("image"), hasDescription, postController.store);
+router.patch("/:id", hasDescription, postController.update);
+router.delete("/:id", postController.delete);
 
 module.exports = router;
